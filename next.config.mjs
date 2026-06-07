a58b6fac29f96af6ae0d57cb7191e8394e9c9d98
haya20221108-sketch <haya20221108@gmail.com>
2026-06-07 10:57:36 +0900

    ver5.1（写真追加）

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development' // 開発環境では無効化
+});
+
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  poweredByHeader: false,
-  images: {
-    unoptimized: true,
-  },
+  // 既存の設定...
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
